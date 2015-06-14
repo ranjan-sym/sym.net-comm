3ad861fda41338642d9ba4f9537cd81772b67c25
Ranjan Shrestha <ranjan@rts.com.np>
2015-06-14 18:06:35 +0545

    Added serial port parameters

--- src/main/java/net/symplifier/comm/serial/SerialPort.java
+++ src/main/java/net/symplifier/comm/serial/SerialPort.java
@@ -16,51 +16,12 @@ import java.nio.ByteBuffer;
 /**
  * Created by ranjan on 6/9/15.
  */
-public class SerialPort extends DigitalPort implements SerialPortEventListener, HasAttribute {
+public class SerialPort extends DigitalPort implements SerialPortEventListener {
 
-  private AttributeManager attributeManager = new AttributeManager();
-
-  public final static AttributeOptions<Integer> BAUD_RATE_OPTIONS = new AttributeOptions<Integer>() {{
-    add(jssc.SerialPort.BAUDRATE_1200, "1200");
-    add(jssc.SerialPort.BAUDRATE_4800, "4800");
-    add(jssc.SerialPort.BAUDRATE_9600, "9600");
-    add(jssc.SerialPort.BAUDRATE_19200, "19200");
-    add(jssc.SerialPort.BAUDRATE_38400, "38400");
-    add(jssc.SerialPort.BAUDRATE_57600, "57600");
-    add(jssc.SerialPort.BAUDRATE_115200, "115200");
-    add(jssc.SerialPort.BAUDRATE_256000, "256000");
-  }};
-
-  public final static AttributeOptions<Integer> DATA_BIT_OPTIONS = new AttributeOptions<Integer>(){{
-    add(jssc.SerialPort.DATABITS_8, "8");
-    add(jssc.SerialPort.DATABITS_7, "7");
-    add(jssc.SerialPort.DATABITS_6, "6");
-    add(jssc.SerialPort.DATABITS_5, "5");
-  }};
-
-  public final static AttributeOptions<Integer> STOP_BITS_OPTIONS = new AttributeOptions<Integer>(){{
-    add(jssc.SerialPort.STOPBITS_1, "1");
-    add(jssc.SerialPort.STOPBITS_1_5, "1.5");
-    add(jssc.SerialPort.STOPBITS_2, "2");
-  }};
-
-  public final static AttributeOptions<Integer> PARITY_OPTIONS = new AttributeOptions<Integer>() {{
-    add(jssc.SerialPort.PARITY_NONE, "None");
-    add(jssc.SerialPort.PARITY_EVEN, "Even");
-    add(jssc.SerialPort.PARITY_ODD, "Odd");
-    add(jssc.SerialPort.PARITY_MARK, "Mark");
-    add(jssc.SerialPort.PARITY_SPACE, "Space");
-  }};
-
-  private Attribute.Integer baudRate = new Attribute.Integer(this, "general", "Baud Rate", jssc.SerialPort.BAUDRATE_9600, BAUD_RATE_OPTIONS);
-  private Attribute.Integer dataBits = new Attribute.Integer(this, "general", "Data Bits", jssc.SerialPort.DATABITS_8, DATA_BIT_OPTIONS);
-  private Attribute.Integer stopBits = new Attribute.Integer(this, "general", "Stop Bits", jssc.SerialPort.STOPBITS_1, STOP_BITS_OPTIONS);
-  private Attribute.Integer parity = new Attribute.Integer(this, "general", "Parity", jssc.SerialPort.PARITY_NONE, PARITY_OPTIONS);
-
-  @Override
-  public AttributeManager getAttributeManager() {
-    return attributeManager;
-  }
+  private int baudRate = jssc.SerialPort.BAUDRATE_9600;
+  private int dataBits = jssc.SerialPort.DATABITS_8;
+  private int stopBits = jssc.SerialPort.STOPBITS_1;
+  private int parity = jssc.SerialPort.PARITY_NONE;
 
   @Override
   public void serialEvent(SerialPortEvent serialPortEvent) {
@@ -144,13 +105,30 @@ public class SerialPort extends DigitalPort implements SerialPortEventListener, 
     port = new jssc.SerialPort(name);
   }
 
+  public SerialPort setParameters(int baudRate, int dataBits, int stopBits, int parity) {
+    this.baudRate = baudRate;
+    this.dataBits = dataBits;
+    this.stopBits = stopBits;
+    this.parity = parity;
+    if (port.isOpened()) {
+      try {
+        port.setParams(baudRate, dataBits, stopBits, parity);
+      } catch(SerialPortException ex) {
+        System.err.println("This error should not be happening");
+        ex.printStackTrace();;
+      }
+    }
 
+    return this;
+  }
 
+  @Override
   public void open() {
     try {
       port.openPort();
-      port.setParams(baudRate.get(), dataBits.get(), stopBits.get(), parity.get());
+      port.setParams(baudRate, dataBits, stopBits, parity);
       port.addEventListener(this, SerialPortEvent.RXCHAR);
+
       getAttachment().onPortOpen(this);
     } catch(SerialPortException ex) {
       getAttachment().onPortError(this, ex);
